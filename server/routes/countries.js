4b9a5674942d10bf25c5f1fbf0dcaaf36a3b9558
Gabriel Salazar <gabriel.salazar777@gmail.com>
2023-05-17 15:36:30 -0400

    backend done-ish

--- server/routes/countries.js
+++ server/routes/countries.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const Country = require("../models/Country");
+const User = require("../models/User");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
-router.post("/create", (req, res, next) => {
+router.post("/create", isAuthenticated, (req, res, next) => {
   Country.find({ commonName: req.body.commonName })
     .then((foundCountry) => {
       if (!foundCountry) {
         Country.create(req.body)
           .then((createdCountry) => {
+            User.findByIdAndUpdate(req.user._id, {
+              $push: { visitedCountries: createdCountry._id },
+            });
             res.json(createdCountry);
           })
           .catch((err) => {
             console.log(err);
           });
       }
+
+      User.findByIdAndUpdate(req.user._id, {
+        $push: { visitedCountries: foundCountry._id },
+      });
+
       res.json(foundCountry);
     })
     .catch((err) => {
@@ -30,7 +40,5 @@ router.get("/detail/:name", (req, res, next) => {
       console.log(err);
     });
 });
-
-
 
 module.exports = router;
